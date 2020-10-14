11aa7e6034f4a6d4ec3352a9c3e8bfe5f1ed7c79
Tania_Bonilla <t.a.b.a.tania@hotmail.com>
2020-10-14 14:36:31 -0600

    make it

--- DB/Conexion/DB.php
+++ DB/Conexion/DB.php
@@ -2,7 +2,7 @@
 $conexion;
 
 try {
-    $conexion = new PDO('mysql:host=localhost;dbname=db_eduvial', 'root', 'mortadela11');
+    $conexion = new PDO('mysql:host=l3855uft9zao23e2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=l1n3zhkxnpq302ky', 'xodrdnmpglqs6wc8', 'n31zflvwc1kfis4o');
 }
 catch (PDOException $prueba_error){
         echo "Error: " . $prueba_error->getMessage();
