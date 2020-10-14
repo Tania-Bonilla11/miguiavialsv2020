b3e9b5d5897f3bb466de53d5830064a7c395d943
Tania_Bonilla <t.a.b.a.tania@hotmail.com>
2020-10-14 15:22:58 -0600

    z

--- DB/Config/Config.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-include('../../Conexion/DB.php');
-$conexion = new Conexion('l3855uft9zao23e2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com','xodrdnmpglqs6wc8','n31zflvwc1kfis4o','l1n3zhkxnpq302ky');
-?>
--- private/logreg/cerrar.php
+++
@@ -1,8 +0,0 @@
-<?php session_start();
-
-    session_destroy();
-    $_SESSION = array();
-
-    header('location: /miguiavialsv/index.html');
-
-?>
