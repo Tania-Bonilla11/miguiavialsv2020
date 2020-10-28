28a3ee19f83b365adec915230263e6bc15b9b69d
Tania_Bonilla <t.a.b.a.tania@hotmail.com>
2020-10-28 13:44:01 -0600

    make it betteaaaaaaar

--- private/control/principal.php
+++ private/control/principal.php
@@ -3,11 +3,11 @@
 if(isset($_SESSION['usuario'])) {
     //Mostrando vistas segun el provilegio
     if ($_SESSION['privilegio'] == 1) {
-        header('location: ../../../public/vistas/homep.html');
+        header('location: ../../../../../host/public/vistas/homep.html');
     }else if($_SESSION['privilegio'] == 2){
-        header('location: ../../../../public/vistas/homep.html');
+        header('location: ../../../../../../host/public/vistas/homep.html');
     }else if($_SESSION['privilegio'] == 0){
-        header('location: ../../../../public/vistas/home.html');
+        header('location: ../../../../../../host/public/vistas/home.html');
     }
 }else{
     header('location: login.php');
